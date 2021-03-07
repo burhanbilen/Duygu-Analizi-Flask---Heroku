99032664e64bedd539c03d87816dd2c41e139517
Burhan Bilen <34451356+burhanbilen@users.noreply.github.com>
2021-03-07 03:36:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, request, render_template
 from keras.models import load_model
-from keras.preprocessing import sequence
 import numpy as np
 import pickle
 
-model = load_model("model.h5")
-tokenizer = pickle.load(open("tokenizer.pickle", 'rb'))
-
 app = Flask(__name__)
+
+model = load_model("NEWMODEL.h5")
+tokenizer = pickle.load(open("NEWTOKENIZER.pickle", 'rb'))
 
 diller = {0:'Afrikanca',1:'Almanca',2:'Arapça',3:'Arnavutça',4:'Azerbaycan Türkçesi',5:'Belarusça'
          ,6:'Bengalce',7:'Boşnakça',8:'Bulgarca',9:'Burmaca',10:'Danca',11:'Endonezce',12:'Estonyaca'
@@ -16,19 +15,19 @@ diller = {0:'Afrikanca',1:'Almanca',2:'Arapça',3:'Arnavutça',4:'Azerbaycan Tü
          ,27:'Makedonca',28:'Malezya Dili',29:'Norveççe',30:'Portekizce',31:'Romence',32:'Rusça'
          ,33:'Slovakça',34:'Slovence',35:'Sırpça',36:'Tatarca',37:'Tayca',38:'Türkmence',39:'Türkçe'
          ,40:'Ukraynaca',41:'Urduca',42:'Uygurca',43:'Vietnamca',44:'Yunanca',45:'Çekçe',46:'Çince'
-         ,47:'İbranice',48:'İspanyolca',49:'İsveççe',50:'İtalyanca',51:'İzlandaca'}
+         ,47:'İbranice',48:'İngilizce',49:'İspanyolca',50:'İsveççe',51:'İtalyanca',52:'İzlandaca'}
 
 def tahmin_et(metin):
     tokenized = tokenizer.texts_to_sequences([metin])
     try:
-        tahmin = np.argmax(model.predict(tokenized), axis=1)
+        tahmin = model.predict_classes(tokenized)
     except:
         tahmin = "Hata"
     return tahmin
 
 @app.route("/", methods=['GET'])
 def index():
-    return render_template("index.html")    
+    return render_template("index.html")
 
 @app.route('/', methods=['GET','POST'])
 def yazi_al():
@@ -38,12 +37,12 @@ def yazi_al():
         if text.strip() == "" or text == None:
             text = "Metin veya ses dosyası ekleyin."
             return render_template("index.html", sonuc = text)
-    
+
     if not tahmin_et(text) == "Hata":
         dil = diller[tahmin_et(text)[0]]
     else:
         dil = "Dil tanımlanamadı."
-        
+
     return render_template("index.html", sonuc = dil)
 
 if __name__ == "__main__":
